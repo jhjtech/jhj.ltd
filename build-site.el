fe07be2f443dfddd0370479a5574be11756ebd66
Mark Woodhall <mark.woodhall@gmail.com>
2024-10-12 18:00:43 +0100

    First version

---
+++ build-site.el
@@ -0,0 +1,71 @@
+;;; build-site.el --- JHJ Company Website Build -*- lexical-binding: t -*-
+
+;; Copyright © 2024-2024 Mark Woodhall and contributors
+
+;;; Commentary:
+
+;; A build script to gernate JHJ compnay website from org mode
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+
+;;; Code:
+(require 'ox-publish)
+
+;; Set the package installation directory so that packages aren't stored in the
+;; ~/.emacs.d/elpa path.
+(require 'package)
+(setq package-user-dir (expand-file-name "./.packages"))
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+;; Initialize the package system
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Install dependencies
+(package-install 'htmlize)
+
+;; Define the publishing project
+(setq org-publish-project-alist
+      (list
+       (list "jhj-org-site"
+             :recursive t
+             :base-directory "./content"
+             :with-author t           ;; Don't include author name
+             :with-creator t            ;; Include Emacs and Org versions in footer
+             :with-toc nil                ;; Include a table of contents
+             :section-numbers nil       ;; Don't include section numbers
+             :publishing-directory "./public"
+             :publishing-function 'org-html-publish-to-html)))
+
+;; Customize the HTML output
+(setq org-html-validation-link nil            ;; Don't show validation link
+      org-html-head-include-scripts nil       ;; Use our own scripts
+      org-html-head-include-default-style nil ;; Use our own styles
+      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
+
+;; Generate the site output
+(org-publish-all t)
+
+(message "Build complete")
+
+
+
+;;; build-site.el ends here
